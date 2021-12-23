b5cb0b5f6addc139bcae72a66508319fae3e0573
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-12-24 03:59:16 +0700

    add crud parent

--- admin/orang-tua/index.php
+++ admin/orang-tua/index.php
@@ -1,5 +1,5 @@
-<?php 
-require_once('check-login.php');
+<?php
+require_once('../check-login.php');
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,7 @@ require_once('check-login.php');
     <title>Goes To School</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../../styles/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -21,27 +21,31 @@ require_once('check-login.php');
 
     <div class="container-fluid content flex-grow-1 d-flex flex-column justify-content-center">
         <header class="text-center mt-4">
-            <h1>Goes to School</h1>
+            <h1>Parent Page</h1>
         </header>
 
         <section class="mb-4 d-flex flex-column align-items-center justify-content-center">
             <article class="col-md-4 text-center">
-                <a href="./siswa/index.php" class="btn btn-primary flex">
-                    <i class="fas fa-graduation-cap btn-icon"></i>
-                    <p class="m-0 mt-3">Students</p>
+                <a href="./tambah-orang-tua.php" class="btn btn-primary flex">
+                    <i class="fas fa-user-plus btn-icon"></i>
+                    <p class="m-0 mt-3">Add Parent</p>
                 </a>
-                <a href="#" class="btn btn-primary flex">
-                    <i class="fas fa-chalkboard-teacher btn-icon"></i>
-                    <p class="m-0 mt-3">Teacher</p>
-                </a>
-                <a href="./orang-tua/index.php" class="btn btn-primary">
-                    <i class="fas fa-user-friends btn-icon"></i>
-                    <p class="m-0 mt-3">Parent</p>
+                <a href="./list-orang-tua.php" class="btn btn-primary flex">
+                    <i class="fas fa-clipboard-list btn-icon"></i>
+                    <p class="m-0 mt-3">Parents List</p>
                 </a>
             </article>
         </section>
 
-
+        <?php
+        if (isset($_GET['status'])) {
+            if ($_GET['status'] == 'gagal' && isset($_GET['msg'])) {
+                echo "<p class=\"text-danger text-center\">$_GET[msg]</p>";
+            } else if ($_GET['status'] == 'berhasil') {
+                echo "<p class=\"text-success text-center\">Succesfully added parent</p>";
+            }
+        }
+        ?>
     </div>
 
 </body>
--- migrations/parents.sql
+++ migrations/parents.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `students` (
+CREATE TABLE `parents` (
     `id` INT NOT NULL AUTO_INCREMENT , 
-    `nis` VARCHAR(5) NOT NULL ,   
     `name` VARCHAR(255) NOT NULL ,  
     `gender` VARCHAR(16) NOT NULL ,  
     `date_of_birth` DATE NOT NULL ,  
     `address` VARCHAR(255) NOT NULL ,  
-    `photo` varchar(200) NOT NULL DEFAULT 'uploaded_images/default.jpg',    
+    `phone_number` VARCHAR(16) NOT NULL, 
+    `parent_of` INT NOT NULL,  
     PRIMARY KEY  (`id`),
-    UNIQUE (`nis`)
+    FOREIGN KEY (`parent_of`) REFERENCES `students`(`id`)
 ) ENGINE = InnoDB;
